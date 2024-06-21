c99034cdcd26ac05888614db4e46944776ecc6a9
Art_m <artem_madrid@hotmail.com>
2024-06-21 11:42:55 +0200

    contact update

--- interactive_helper/contacts/forms.py
+++ interactive_helper/contacts/forms.py
@@ -4,11 +4,11 @@ from .models import Contact
 
 class ContactForm(ModelForm):
     id        = IntegerField(widget=HiddenInput(), required=False)
-    fullname  = CharField(min_length=3, max_length=25, widget=TextInput(attrs={"class": "form-control"}))
-    email     = EmailField(widget=EmailInput(attrs={"class": "form-control"}))
+    fullname  = CharField(required=True, min_length=3, max_length=25, widget=TextInput(attrs={"class": "form-control"}))
+    email     = EmailField(required=True, widget=EmailInput(attrs={"class": "form-control"}))
     tel_num   = IntegerField(required=True, widget=NumberInput(attrs={"class": "form-control"}))
-    born_date = DateField(widget=DateInput(attrs={"type": "date", "class": "form-control"}))
-    address   = CharField(widget=TextInput(attrs={"class": "form-control"}))
+    born_date = DateField(required=True, widget=DateInput(attrs={"type": "date", "class": "form-control"}))
+    address   = CharField(required=True, widget=TextInput(attrs={"class": "form-control"}))
 
 
     class Meta:
