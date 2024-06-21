03a927e6e839db6435c61d82fbcbc2ef541bd193
Art_m <artem_madrid@hotmail.com>
2024-06-21 11:59:04 +0200

    notes update

--- interactive_helper/contacts/models.py
+++ interactive_helper/contacts/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    id        = models.AutoField(primary_key=True)
-    fullname  = models.CharField(unique=True, max_length=20)
-    tel_num   = models.IntegerField(unique=True, max_length=15)
-    email     = models.EmailField(unique=True)
+    id       = models.AutoField(primary_key=True)
+    fullname = models.CharField(unique=True, max_length=20)
+    email    = models.EmailField(unique=True)
+    tel_num  = models.IntegerField(unique=True, max_length=15)
     born_date = models.DateField(null=False)
     address   = models.TextField(null=False)
 
--- interactive_helper/notes/models.py
+++ interactive_helper/notes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Contact(models.Model):
+    id       = models.AutoField(primary_key=True)
+    fullname = models.CharField(unique=True, max_length=20)
+    email    = models.EmailField(unique=True)
+    tel_num  = models.IntegerField(unique=True, max_length=15)
+    born_date = models.DateField(null=False)
+    address   = models.TextField(null=False)
+
+    def __str__(self):
+        return f"{self.fullname}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Note(models.Model):
+    id      = models.AutoField(primary_key=True)
+    note    = models.TextField(unique=True, null=False)
+    contact = models.ForeignKey(Contact, to_field="id", on_delete=models.CASCADE)
+    tags    = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.tags}"
