8e34be29bce8471101c8ada3162a9bbf4e1148af
FyntikUA <113473836+FyntikUA@users.noreply.github.com>
2024-06-30 14:27:50 +0300

    Update settings.py

--- interactive_helper/interactive_helper/settings.py
+++ interactive_helper/interactive_helper/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+# Завантаження змінних середовища
+CLIENT_ID = config('CLIENT_ID')
+CLIENT_SECRET = config('CLIENT_SECRET')
+TOKEN_URI = config('TOKEN_URI')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
