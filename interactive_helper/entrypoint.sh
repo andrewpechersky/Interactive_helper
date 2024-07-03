06eb868e87cc322f1cc371cfab54f9a6bb34a4a4
Andrew Pechersky <andrewpechersky@gmail.com>
2024-07-03 10:48:19 +0300

    ip upd

--- interactive_helper/entrypoint.sh
+++ interactive_helper/entrypoint.sh
@@ -4,4 +4,4 @@
 python manage.py collectstatic --noinput
 
 # Запуск сервера
-gunicorn interactive_helper.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn interactive_helper.wsgi:application --bind 127.0.0.1:$PORT
